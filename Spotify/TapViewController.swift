19a92c8ecfe27ed801fa2c44eb2e6828ff024bdb
SWpewpew <ini@MacBook-Pro-ini.local>
2016-12-17 14:20:27 +0300

    Changed location of classes

---
+++ Spotify/TapViewController.swift
@@ -0,0 +1,38 @@
+
+
+import UIKit
+
+class TapViewController: UIViewController {
+
+  //  @IBOutlet weak var picture: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+   //     picture.isUserInteractionEnabled = true
+    //    let TapGesture = UITapGestureRecognizer(target : self, action :  #selector(TapViewController.ImageTapped))
+    //    self.picture.addGestureRecognizer(TapGesture)
+        // Do any additional setup after loading the view.
+    }
+    
+   // func ImageTapped()  {
+        
+   //     performSegue(withIdentifier: "segueX", sender: nil)
+   // }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
