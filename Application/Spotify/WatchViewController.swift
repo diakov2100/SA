6af10c731992b48978ebe3e6824d8ad0ea787991
prosolkin <prosolkin@bk.ru>
2016-12-18 21:34:10 +0300

    prefinal build

---
+++ Application/Spotify/WatchViewController.swift
@@ -0,0 +1,69 @@
+import UIKit
+import WatchConnectivity
+
+
+class WatchViewController: UIViewController {
+    
+    @IBOutlet weak var heartLabel: UILabel!
+    
+    fileprivate let session: WCSession? = WCSession.isSupported() ? WCSession.default() : nil
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        configureWCSession()
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        
+        configureWCSession()
+    }
+    
+    fileprivate func configureWCSession() {
+        session?.delegate = self
+        session?.activate()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
+extension WatchViewController: WCSessionDelegate {
+    /** Called when all delegate callbacks for the previously selected watch has occurred. The session can be re-activated for the now selected watch using activateSession. */
+    @available(iOS 9.3, *)
+    public func sessionDidDeactivate(_ session: WCSession) {
+        //Dummy Implementation
+    }
+    
+    /** Called when the session can no longer be used to modify or add any new transfers and, all interactive messages will be cancelled, but delegate callbacks for background transfers can still occur. This will happen when the selected watch is being changed. */
+    @available(iOS 9.3, *)
+    public func sessionDidBecomeInactive(_ session: WCSession) {
+        //Dummy Implementation
+    }
+    
+    /** Called when the session has completed activation. If session state is WCSessionActivationStateNotActivated there will be an error with more details. */
+    @available(iOS 9.3, *)
+    public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        //Dummy Implementation
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        
+        //Use this to update the UI instantaneously (otherwise, takes a little while)
+        DispatchQueue.main.async {
+            if let pulseValue = message["pulseValue"] as? String {
+                self.heartLabel.text = pulseValue
+            }
+        }
+    }
+}
+
+
